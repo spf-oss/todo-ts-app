d911e320cd56a5b338d6d1a6fb3e07c6b32741b0
spf <1771245717@qq.com>
2022-07-02 22:46:06 +0800

    TodoList组件

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="todo-list">
+    <todo-list-item 
+    v-for="todo in todos"
+    :key="todo.id"
+    @change-state="todo.completed == $event.target.checked" 
+    :todo-item="todo">
+    </todo-list-item>
+  </div>
+</template>
+
+<script lang="ts">
+import ITodoListItem from '@/composables/ITodoListItem';
+import { defineComponent, PropType } from 'vue'
+import TodoListItem from './TodoListItem.vue'
+
+export default defineComponent({
+  components: { TodoListItem },
+  setup() {
+    return {};
+  },
+  props:{
+    todos:{
+      type:Object as PropType<ITodoListItem[]>
+    }
+  }
+})
+</script>
+
+<style>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
